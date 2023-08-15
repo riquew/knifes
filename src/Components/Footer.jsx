d825958c49fa7d0c32cf98fc7ae26d5411a595fa
riquew <hwindlin@gmail.com>
2023-08-15 01:50:11 -0300

    Entrega footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import styles from "./Footer.module.css";
+import logoMadeira from "../img/logoMadeira.png";
+import instagram from "../img/instagram.svg";
+import facebook from "../img/facebook.svg";
+import youtube from "../img/youtube.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={"container " + styles.content}>
+        <div>
+          <ul className={styles.contato}>
+            <p>CONTATO</p>
+            <li>(11) 99999 - 9999</li>
+            <li>domfacone@gmail.com</li>
+            <li>Rua Barao Rio Preto, 813</li>
+            <li>Jundiai - SP</li>
+          </ul>
+          <ul className={styles.socialMedias}>
+            <li>
+              <a href="">
+                <img src={instagram} alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src={facebook} alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src={youtube} alt="" />
+              </a>
+            </li>
+            <li></li>
+          </ul>
+        </div>
+
+        <img src={logoMadeira} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
