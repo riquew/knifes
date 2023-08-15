89693195c236ff7d8c423f29012222cfc8a783e5
riquew <hwindlin@gmail.com>
2023-08-14 22:56:28 -0300

    Inicia projeto"

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
