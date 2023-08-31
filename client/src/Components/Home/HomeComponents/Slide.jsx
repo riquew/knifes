7b12104874e34399ffe6ec1d2766f99cff1db4cc
riquew <hwindlin@gmail.com>
2023-08-31 17:56:27 -0300

    Modifica imagens slide

--- client/src/Components/Home/HomeComponents/Slide.jsx
+++ client/src/Components/Home/HomeComponents/Slide.jsx
@@ -1,40 +1,64 @@
 import React from "react";
-import slide1 from "../../../img/slide/slide13.jpg";
+import foto0 from "../../../img/slide/slide0.jpg";
+import foto1 from "../../../img/slide/slide1.jpg";
+import foto2 from "../../../img/slide/slide2.jpg";
 import styles from "./Slide.module.css";
 
 const Slide = () => {
+  let styleSlide = {
+    marginBottom: `900px`,
+    background: `url(${foto0})`,
+    backgroundSize: `cover`,
+    color: `#fff`,
+  };
+
   const listaTexto = [
-    { index: 0, titulo: "AFIADA", subTitulo: "A afiação que você precisa." },
+    {
+      index: 0,
+      titulo: "AFIADA",
+      subTitulo: "A afiação que você precisa.",
+      foto: foto0,
+    },
     {
       index: 1,
       titulo: "PRECISA",
       subTitulo: "A precisão necessária para as tarefas diárias",
+      foto: foto1,
     },
-    { index: 2, titulo: "CORTANTE", subTitulo: "O melhor corte do país" },
+    {
+      index: 2,
+      titulo: "CORTANTE",
+      subTitulo: "O melhor corte do país",
+      foto: foto2,
+    },
   ];
 
   const [textoSlide, setTextoSlide] = React.useState(listaTexto[0]);
 
-  function changeText() {
-    console.log(textoSlide.index);
-    if (textoSlide.index === 2) {
+  function changeText(action) {
+    console.log(styleSlide.background);
+    if (textoSlide.index === 2 && action === "forward") {
       setTextoSlide(listaTexto[0]);
-    } else {
+    } else if (textoSlide.index === 0 && action === "back") {
+      setTextoSlide(listaTexto[2]);
+    } else if (action === "forward") {
       setTextoSlide(listaTexto[textoSlide.index + 1]);
+    } else {
+      setTextoSlide(listaTexto[textoSlide.index - 1]);
     }
   }
 
   function goBack() {
+    // slide.current.style.backgroundImage = "url(../../../img/slide/slide0.jpg)";
+    console.log(slide.current);
     slide.current.style.backgroundPositionX = +window.innerWidth;
-    // titulo.current.innerText = "TESTE TITULO VOLTA";
-    // subTitulo.current.innerText = "TESTE SUBTITULO VOLTA";
+    changeText("back");
   }
 
   function goForward() {
+    // slide.current.style.backgroundImage = "url(../../../img/slide/slide2.jpg)";
     slide.current.style.backgroundPositionX = -window.innerWidth;
-    changeText();
-    // titulo.current.innerText = "TESTE TITULO VAI";
-    // subTitulo.current.innerText = "TESTE SUBTITULO VAI";
+    changeText("forward");
   }
 
   const slide = React.useRef(null);
@@ -44,7 +68,7 @@ const Slide = () => {
   console.log(window.innerWidth);
 
   return (
-    <div className={styles.slide} ref={slide}>
+    <div style={styleSlide} ref={slide}>
       <div className={"container"}>
         <div className={styles.slideTexto}>
           <h1 ref={titulo}>{textoSlide.titulo}</h1>
